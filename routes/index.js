acf32d1db4ff9d7a117360037b4060b5060244c7
Muhammed Ekinci <siraceddinekinci@gmail.com>
2025-03-23 16:27:01 -0400

    added nodemon

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // Sample route
 router.get("/", (req, res) => {
   res.json({ message: "Welcome to your Node.js server!" });
-});
+}); ///
 
 module.exports = router;
